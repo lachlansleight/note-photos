2f31230b24788521be4f409dcb309bf0c3e1f8f0
LachlanSleight <lachlansleight@gmail.com>
2025-03-08 16:28:37 +0000

    feat: Make transcription more reliable + useful

--- pages/api/receiveTranscribe.ts
+++ pages/api/receiveTranscribe.ts
@@ -17,6 +17,14 @@ api.post = async (req, res) => {
 
     const idToken = String(req.query.auth || "");
     console.log("Transcription: ", transcriptionData);
+    if(!transcriptionData) {
+        res.status(400).json({ error: "No transcription data provided" });
+        return;
+    }
+    if(!transcriptionData.tagline?.trim()?.length) {
+        res.status(400).json({ error: "No tagline provided" });
+        return;
+    }
     const response = await patchImage(image.id, { transcription: transcriptionData }, idToken);
     res.status(200).json(response);
 };
--- pages/api/transcribe.ts
+++ pages/api/transcribe.ts
@@ -4,32 +4,36 @@ import { getImage } from "./image";
 import { NoteImage } from "lib/types";
 import axios from "axios";
 
-export const getParabrainTranscription = async (image: NoteImage) => {
+export const getParabrainTranscription = async (image: NoteImage, idToken: string) => {
     //todo - parabrain needs a receive post request first, otherwise I can't send the whole noteImage
     //OR DOES IT >:D
 
     //...yes it does. I'm back now I made it work
 
     console.log("POSTing parabrain...");
-    const response = (await axios.post("http://localhost:4937/message?id=transcribeImage", image));
+    const response = (await axios.post("http://localhost:4937/message?id=transcribeImage", {image, idToken}));
     return response.data;
 }
 
 const api = new NextRestApiRoute("/transcribe");
 api.get = async (req, res) => {
     const image: NoteImage = await getImage(req.query.id as string);
+    const idToken = req.query.auth as string;
     if (!image) {
+        console.error("Image not found for ID " + req.query.id);
         res.status(404).json({ error: "Image not found" });
         return;
     }
 
     const forceEvenIfTranscribed = req.query.force === "true";
-    if (image.transcription && !forceEvenIfTranscribed) {
+    const imageHasTranscription = image.transcription && image.transcription.tagline?.trim()?.length;
+    if (imageHasTranscription && !forceEvenIfTranscribed) {
+        console.log("Image is already transcribed and force mode is off");
         res.json(image.transcription);
         return;
     }
 
-    const transcriptionData = await getParabrainTranscription(image);
+    const transcriptionData = await getParabrainTranscription(image, idToken);
     res.status(200).json(transcriptionData);
 };
 
